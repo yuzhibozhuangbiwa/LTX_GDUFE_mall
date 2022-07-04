c62443b84ddbb042fbdbd0c8a251ccfbc4a57a00
林廷炫 <10829324+lin-tingxuan@user.noreply.gitee.com>
2022-07-04 17:31:14 +0800

    77777

--- src/main/java/ltd/newbee/mall/controller/common/NewBeeMallExceptionHandler.java
+++ src/main/java/ltd/newbee/mall/controller/common/NewBeeMallExceptionHandler.java
@@ -46,8 +46,8 @@ public class NewBeeMallExceptionHandler {
             modelAndView.addObject("message", e.getMessage());
             modelAndView.addObject("url", req.getRequestURL());
             modelAndView.addObject("stackTrace", e.getStackTrace());
-            modelAndView.addObject("author", "十三");
-            modelAndView.addObject("ltd", "新蜂商城");
+            modelAndView.addObject("author", "ltx");
+            modelAndView.addObject("ltd", "gdufe商城");
             modelAndView.setViewName("error/error");
             return modelAndView;
         }
